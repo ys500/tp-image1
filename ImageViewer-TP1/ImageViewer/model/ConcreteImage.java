d06523d93bb2f9b1dfffe489e2fc96f8cce79ef4
ys100 <youssef.soliman.1@ens.etsmtl.ca>
2018-02-01 18:33:28 -0500

    image affichee

--- ImageViewer-TP1/ImageViewer/model/ConcreteImage.java
+++ ImageViewer-TP1/ImageViewer/model/ConcreteImage.java
@@ -1,5 +1,6 @@
 package model;
 
+import java.awt.Color;
 import java.awt.image.BufferedImage;
 import java.io.BufferedInputStream;
 import java.io.File;
@@ -8,7 +9,8 @@ import java.io.FileNotFoundException;
 import java.io.IOException;
 
 public class ConcreteImage extends Image{
-	private final byte TAILLE_ENTETE = 54;
+	private final static byte TAILLE_ENTETE = 54;
+	private final static int DEBUT_DONNEES_IMAGE = 55;
 	private final int MASK = 0b11111111;
 
 	BufferedInputStream fileReader;
@@ -19,6 +21,7 @@ public class ConcreteImage extends Image{
 	private int width;
 	private int bpp;
 	private int compression;
+	private int size;
 		
 	public ConcreteImage(File file) {
 			try {
@@ -31,33 +34,71 @@ public class ConcreteImage extends Image{
 			} catch (IOException e) {
 				e.printStackTrace();
 			}
-			/*for (int i = 0; i < tabEtiquette.length; i++) {
-				System.out.println("byte #" + i + " : " + tabEtiquette[i]);
-			}*/
-			//width: tabEtiquette[18] a tabEtiquette[21](4 octets)
-			width = tabEtiquette[18] & MASK | 
-					((tabEtiquette[19] & MASK) << 8) | 
-					((tabEtiquette[20] & MASK) << 16) |
-					((tabEtiquette[21] & MASK) << 24);
-			//height: tabEtiquette[22] a tabEtiquette[25](4 octets)
-	        height = tabEtiquette[22] & MASK | 
-	        		((tabEtiquette[23] & MASK) << 8) | 
-	        		((tabEtiquette[24] & MASK) << 16) | 
-	        		((tabEtiquette[25] & MASK) << 24);
-	        //bpp/bitCount:  tabEtiquette[28] et tabEtiquette[29](2 octets)
-	        bpp = tabEtiquette[28] & MASK | 
-					((tabEtiquette[29] & MASK) << 8);
-	        //compression:tabEtiquette[30] a tabEtiquette[33](4 octets)
-	        compression = tabEtiquette[30] & MASK | 
-	        		((tabEtiquette[31] & MASK) << 8) | 
-	        		((tabEtiquette[32] & MASK) << 16) | 
-	        		((tabEtiquette[33] & MASK) << 24);
+			
+			initDonneesEntete();
+			
 			//seulement supporter les images BMP non-compresse de 24 bits par pixels
 			if(compression == 0 && bpp == 24) {
 				image = new BufferedImage(width, height, BufferedImage.TYPE_INT_RGB);
+				dessinerImage();
 			}
 	}
 	
+	private void initDonneesEntete() {
+		//width: tabEtiquette[18] a tabEtiquette[21](4 octets)
+		width = tabEtiquette[18] & MASK | 
+				((tabEtiquette[19] & MASK) << 8) | 
+				((tabEtiquette[20] & MASK) << 16) |
+				((tabEtiquette[21] & MASK) << 24);
+		//height: tabEtiquette[22] a tabEtiquette[25](4 octets)
+        height = tabEtiquette[22] & MASK | 
+        		((tabEtiquette[23] & MASK) << 8) | 
+        		((tabEtiquette[24] & MASK) << 16) | 
+        		((tabEtiquette[25] & MASK) << 24);
+        //bpp/bitCount:  tabEtiquette[28] et tabEtiquette[29](2 octets)
+        bpp = tabEtiquette[28] & MASK | 
+				((tabEtiquette[29] & MASK) << 8);
+        //compression:tabEtiquette[30] a tabEtiquette[33](4 octets)
+        compression = tabEtiquette[30] & MASK | 
+        		((tabEtiquette[31] & MASK) << 8) | 
+        		((tabEtiquette[32] & MASK) << 16) | 
+        		((tabEtiquette[33] & MASK) << 24);
+      //size:tabEtiquette[34] a tabEtiquette[37](4 octets)
+        size = tabEtiquette[34] & MASK | 
+        		((tabEtiquette[35] & MASK) << 8) | 
+        		((tabEtiquette[36] & MASK) << 16) | 
+        		((tabEtiquette[37] & MASK) << 24);
+	}
+	private void dessinerImage() {
+		tabEtiquette = new byte[DEBUT_DONNEES_IMAGE + size];
+		try {
+			fileReader.read(tabEtiquette, 0, DEBUT_DONNEES_IMAGE + size);
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+		int position = 0;
+		
+		for (int y = height - 1; y >= 0; y--) {
+			for (int x = 0; x < width; x++) {
+				int r = convert(tabEtiquette[position]);
+				int v = convert(tabEtiquette[position + 1]);
+				int b = convert(tabEtiquette[position + 2]);
+				position += 3;
+				Color rvb = new Color(r, v, b);
+				image.setRGB(x, y, rvb.getRGB());
+			}
+		}
+	}
+	private int convert(int couleur) {
+		if (couleur == -1) {
+			return 254;
+		} else if (couleur < 0) {
+			return couleur + 128;
+		} else {
+			return couleur;
+		}
+	}
 	@Override
 	public BufferedImage draw() {
 		return image;
